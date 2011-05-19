5a9619b32b7d8d9805572b06a1ae3a596dd13b97
claydodo <claydodo@localhost>
2011-05-19 16:14:49 +0800

    lidar.py works now

--- metlib/lidar/lidar.py
+++ metlib/lidar/lidar.py
@@ -6,46 +6,42 @@
     TODO: add support for loading short lines.
 """
 
-import os, sys
-#import re
 from datetime import datetime, timedelta
 import numpy as np
-#import scipy as sp
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.basemap import Basemap
-#from matplotlib import mlab
 from netCDF4 import Dataset
+
+_std_datetime_fmt = "%Y-%m-%d %H:%M:%S"
+_3d_time_channel_bin_varnames = ( ('data', 'f4'),
+        )
+_1d_time_varnames = ( ('datetime', str),
+        ('shots_sum', 'u4'),
+        ('trigger_frequency', 'i4'),
+#            ('dead_time_corrected', 'u2'),
+        ('azimuth_angle', 'f4'),
+        ('elevation_angle', 'f4'),
+        ('cloud_base_height', 'f4'),
+        )
+_1d_bin_varnames = ( ('distance', 'f4'),
+        )
+_2d_time_channel_varnames = ( ('background', 'f4'),
+        ('background_std_dev', 'f4'),
+        ('energy', 'f4'),
+        )
+_2d_time_other_varnames = (('temp', 'f4', 'TEMP'), 
+        )
+_0d_attrs = ( ('lidarname', str),
+        ('bin_time', 'f4'),
+        ('bin_size', 'f4'),
+        ('first_data_bin', 'i4'),
+        ('start_datetime', str),
+        ('end_datetime', str),
+        ('number_records', 'i4'),
+        ('number_bins', 'i4'),
+        ('number_channels', 'i4'),
+        )
 
 class LidarDataset(object):
     """doc"""
-    _std_datetime_fmt = "%Y-%m-%d %H:%M:%S"
-    _3d_time_channel_bin_varnames = ( ('data', 'f4'),
-            )
-    _1d_time_varnames = ( ('datetime', str),
-            ('distance', 'f4'),
-            ('shots_sum', 'u4'),
-            ('trigger_frequency', 'i4'),
-#            ('dead_time_corrected', 'u2'),
-            ('azimuth_angle', 'f4'),
-            ('elevation_angle', 'f4'),
-            ('cloud_base_height', 'f4'),
-            )
-    _2d_time_channel_varnames = ( ('background', 'f4'),
-            ('background_std_dev', 'f4'),
-            ('energy', 'f4'),
-            )
-    _2d_time_other_varnames = (('temp', 'f4'), 
-            )
-    _0d_attrs = ( ('lidarname', str),
-            ('bin_time', 'f4'),
-            ('bin_size', 'f4'),
-            ('first_data_bin', 'i4'),
-            ('start_datetime', str),
-            ('end_datetime', str),
-            ('number_records', 'i4'),
-            ('number_bins', 'i4'),
-            ('number_channels', 'i4'),
-            )
     def __init__(self, fnames, **kwargs):
         """doc
         kwargs:
@@ -53,6 +49,7 @@ class LidarDataset(object):
         """
         self.orig_fnames = []
         self._vars_inited = False
+        self.dims = dict()
         self.append_files(fnames, **kwargs)
     
     def append_files(self, fnames, bin_num=None):
@@ -81,7 +78,7 @@ class LidarDataset(object):
             bin_num = np.min((orig_bin_nums.min(), bin_num))
         proper_dims['BIN'] = bin_num
         proper_dims['TIME'] = record_nums.sum()
-        
+        self.dims.update(proper_dims)
         # # create the arrays for each var
         for vname, t in _3d_time_channel_bin_varnames:
             if t is str:
@@ -91,30 +88,38 @@ class LidarDataset(object):
             if t is str:
                 t = 'O'
             _tmp_pool[vname] = np.zeros((proper_dims['TIME'], proper_dims['CHANNEL']), dtype=t)
-        for vname, t in _2d_time_other_varnames:
+        for vname, t, dname in _2d_time_other_varnames:
             if t is str:
                 t = 'O'
-            # # TODO: how to deal with this?
+            _tmp_pool[vname] = np.zeros((proper_dims['TIME'], proper_dims[dname]), dtype=t)
         for vname, t in _1d_time_varnames:
             if t is str:
                 t = 'O'
             _tmp_pool[vname] = np.zeros((proper_dims['TIME'], ), dtype=t)
+        for vname, t in _1d_bin_varnames:
+            if t is str:
+                t = 'O'
+            _tmp_pool[vname] = np.zeros((proper_dims['BIN'], ), dtype=t)
         
-        end_is = record_nums.cumsum()
-        start_is = end_is - record_nums[0]
+        start_is = np.hstack((0, record_nums.cumsum()[:-1]))
+        end_is = start_is + record_nums
         for i, fname in enumerate(fnames):
             f = Dataset(fname)
+            if i == 0:
+                for vname, t in _1d_bin_varnames:
+                    _tmp_pool[vname][:] = f.variables[vname][:proper_dims['BIN']]
             for vname, t in _3d_time_channel_bin_varnames:
                 _tmp_pool[vname][start_is[i]:end_is[i],:,:] = f.variables[vname][:,:,:proper_dims['BIN']]
             for vname, t in _2d_time_channel_varnames:
                 _tmp_pool[vname][start_is[i]:end_is[i],:] = f.variables[vname][:]
-            for vname, t in _2d_time_other_varnames:
-                #_tmp_pool[vname][start_is[i]:end_is[i],:] = f.variables[vname][:]
-                #TODO
-                pass
+            for vname, t, dname in _2d_time_other_varnames:
+                _tmp_pool[vname][start_is[i]:end_is[i]] = f.variables[vname][:]
             for vname, t in _1d_time_varnames:
                 _tmp_pool[vname][start_is[i]:end_is[i]] = f.variables[vname][:]
             f.close()
+ 
+        _tmp_pool['datetime'] = np.array([datetime.strptime(datestr, _std_datetime_fmt) for datestr in _tmp_pool['datetime']])
+
         for attr, t in _0d_attrs:
             _tmp_pool[attr] = ref_attrs[attr]
         _tmp_pool['start_datetime'] = all_start_datetime
@@ -129,12 +134,11 @@ class LidarDataset(object):
                 self.vars[vname] = np.vstack(self.vars[vname], _tmp_pool[vname])
             for vname, t in _2d_time_channel_varnames:
                 self.vars[vname] = np.vstack(self.vars[vname], _tmp_pool[vname])
-            for vname, t in _2d_time_other_varnames:
-#                self.vars[vname] = np.vstack(self.vars[vname], _tmp_pool[vname])
-            #   TODO
-                pass
+            for vname, t, dname in _2d_time_other_varnames:
+                self.vars[vname] = np.vstack(self.vars[vname], _tmp_pool[vname])
             for vname, t in _1d_time_varnames:
                 self.vars[vname] = np.hstack(self.vars[vname], _tmp_pool[vname])
+
             self.vars['end_datetime'] = _tmp_pool['end_datetime']
             self.vars['number_records'] += _tmp_pool['number_records']
 
@@ -147,14 +151,45 @@ class LidarDataset(object):
             dim_lens[dimname] = len(f.dimensions[dimname])
         for attrname in f.ncattrs():
             attrs[attrname] = f.getncattr(attrname)
-        attrs[start_datetime] = datetime.strptime(attrs[start_datetime], _std_datetime_fmt)
-        attrs[end_datetime] = datetime.strptime(attrs[end_datetime], _std_datetime_fmt)
+        attrs['start_datetime'] = datetime.strptime(attrs['start_datetime'], _std_datetime_fmt)
+        attrs['end_datetime'] = datetime.strptime(attrs['end_datetime'], _std_datetime_fmt)
         return attrs, dim_lens
 
     def save(self, fname):
         """doc"""
-        pass
-
+        f = Dataset(fname, 'w', format='NETCDF4')
+        for dname, length in self.dims.items():
+            if dname == 'TIME':
+                f.createDimension(dname)
+            else:
+                f.createDimension(dname, length)
+        
+        for vname, t in _3d_time_channel_bin_varnames:
+            f.createVariable(vname, t, ('TIME', 'CHANNEL', 'BIN'))
+            f.variables[vname][:] = self.vars[vname]
+        for vname, t in _2d_time_channel_varnames:
+            f.createVariable(vname, t, ('TIME', 'CHANNEL'))
+            f.variables[vname][:] = self.vars[vname]
+        for vname, t, dname in _2d_time_other_varnames:
+            f.createVariable(vname, t, ('TIME', dname))
+            f.variables[vname][:] = self.vars[vname]
+        for vname, t, in _1d_time_varnames:
+            f.createVariable(vname, t, ('TIME',))
+            if vname == 'datetime':
+                for i, dt in enumerate(self.vars[vname]):
+                    f.variables[vname][i] = dt.strftime(_std_datetime_fmt)
+            else:
+                f.variables[vname][:] = self.vars[vname]
+        for vname, t, in _1d_bin_varnames:
+            f.createVariable(vname, t, ('BIN',))
+            f.variables[vname][:] = self.vars[vname]
+    
+        for attr, t in _0d_attrs:
+            if attr in ('start_datetime', 'end_datetime'):
+                f.setncattr(attr, self.vars[attr].strftime(_std_datetime_fmt))
+            else:
+                f.setncattr(attr, self.vars[attr])
+        f.close()
 
 if __name__ == '__main__':
     pass
