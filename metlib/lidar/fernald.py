45ec8d4f5bf6aa9bbe52f43c18d4ded857bf912d
claydodo <claydodo@localhost>
2011-09-07 20:47:31 +0800

    lidar progressing

--- metlib/lidar/fernald.py
+++ metlib/lidar/fernald.py
@@ -8,7 +8,7 @@ import os, sys
 #from datetime import datetime, timedelta
 import numpy as np
 #import scipy as sp
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
 #from matplotlib import mlab
 from metlib.lidar.constant import lidar_sm
@@ -22,22 +22,57 @@ def fernald(data, lidar_constant, lidar_ratio, intbm, C_contains_E=False):
     intbm: \int^{0}_{r}betam(z)*dz.
     """
     dz = data.vars['bin_size']
+    try:
+        start_i = data.vars['first_data_bin']
+    except:
+        start_i = 0
     CE = np.zeros_like(data.vars['data'])
     if C_contains_E:
         CE[:] = lidar_constant
     else:
         CE[:] = lidar_constant * data.vars['energy'].reshape((CE.shape[0],CE.shape[1],1))
 #    print "CE: ", CE.shape
+    plt.figure()
+    plt.pcolormesh(CE[:,0,:])
+    plt.colorbar()
+    plt.title("CE")
+
     expIntBm = np.exp(-2.0*(lidar_ratio-lidar_sm)*intbm)
 #    print "expIntBm: ", expIntBm.shape
+    plt.figure()
+    plt.plot(expIntBm)
+    plt.title("expIntBm")
+
     XexpIntBm = data.vars['data'] * expIntBm
 #    print "XexpIntBm: " , XexpIntBm.shape
-    intXexpIntBm = np.add.accumulate(XexpIntBm * dz, axis=-1)
+    plt.figure()
+    plt.pcolormesh(XexpIntBm[:,0,:])
+    plt.colorbar()
+    plt.title("XexpIntBm")
+
+    intXexpIntBm = np.zeros_like(XexpIntBm)
+    intXexpIntBm[...,start_i:] = np.add.accumulate(XexpIntBm[...,start_i:] * dz, axis=-1)
 #    print "intXexpIntBm: " , intXexpIntBm.shape
+    plt.figure()
+    plt.pcolormesh(intXexpIntBm[:,0,:])
+    plt.colorbar()
+    plt.title("intXexpIntBm")
+
     bottom_term = CE - 2.0 * lidar_ratio * intXexpIntBm
 #    print "bottom_term:" , bottom_term.shape
+    plt.figure()
+    plt.pcolormesh(bottom_term[:,0,:])
+    plt.colorbar()
+    plt.title("bottom")
+
     ba = XexpIntBm / bottom_term
+    ba[...,start_i:] = 0.0
 #    print "ba: ", ba.shape
+    plt.figure()
+    plt.pcolormesh(ba[:,0,:])
+    plt.colorbar()
+    plt.title("ba")
+
     data.vars['data'] = ba
     data.desc += ',retrievaled'
 
