f5ed267c78605cc2953fc1e3221b67f0646b227d
claydodo <claydodo@localhost>
2011-10-10 11:23:23 +0800

    metlib.io.fortran_binary updated

--- metlib/io/fortran_binary.py
+++ metlib/io/fortran_binary.py
@@ -8,59 +8,33 @@ import os, sys
 #from datetime import datetime, timedelta
 import numpy as np
 #import scipy as sp
-from scipy.io.numpyio import fread as sp_fread
+#from scipy.io.numpyio import fread as sp_fread
 #import matplotlib.pyplot as plt
 #from mpl_toolkits.basemap import Basemap
 #from matplotlib import mlab
 
 __all__ = ['FortranBinary']
 class FortranBinary(object):
-    def __init__(self, fname, byteswap=False):
+    def __init__(self, fname, dtype='f8'):
+        self.dtype = np.dtype(dtype)
+        if self.dtype.byteorder == '>':
+            rechead_dtype = '>i4'
+        else:
+            rechead_dtype = 'i4'
+        self.records = []
         self._f = open(fname, 'rb')
-        self._byteswap = 1 if byteswap else 0
-        self._rec_num = 0
         self._f.seek(0,2)
         self._filesize = self._f.tell()
-        self._last_rec_len = 0
         self.rewind()
-        while self._f.tell() != self._filesize :
-            self._bypass_rec()
-            self._rec_num += 1
-        self.rewind()
+        now_pos=0
+        while now_pos != self._filesize :
+            recsize = np.memmap(self._f, dtype=rechead_dtype, mode='c', offset=now_pos, shape=(1,))[0]
+            now_pos += 4   # rechead size
+            self.arrays.append(np.memmap(self._f, dtype=dtype, mode='c', offset=now_pos, shape=(recsize / self.dtype.itemsize,)))
+            now_pos += recsize
+            now_pos += 4   # rechead size
 
     def __len__(self):
-        return self._rec_num
-
-    def read(self, dtype='d', shape=None):
-        if self._f.tell() == self._filesize:
-            return None
-        if dtype in ('d',):
-            s = 8
-        elif dtype in ('i', 'f'):
-            s = 4
-        elif dtype in ('s', ):
-            s = 2
-            #TODO: implement all PyArray types
-        rec_len = self._read_rec_len()
-        tmp = sp_fread(self._f, rec_len / s, dtype, dtype, self._byteswap)
-        self._read_rec_len()
-        if shape is None:
-            return tmp
-        else:
-            return tmp.reshape(shape)
-
-    def _read_rec_len(self):
-        self._last_rec_len = sp_fread(self._f, 1, 'i', 'i', self._byteswap)[0]
-        return self._last_rec_len
-
-    def _bypass_rec(self):
-        rec_len = self._read_rec_len()
-        self._f.seek(rec_len + 4, 1)
-
-    def rewind(self, to_beginning = True):
-        if to_beginning:
-            self._f.seek(0, 0)
-        else:
-            self._f.seek(-(8+self._last_rec_len), 1)
+        return len(self.records)
 
 # TODO: add rec index, etc.
