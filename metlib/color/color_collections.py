b772aca9b01ccfcdc79cb2c8c0657d46352415f2
claydodo <claydodo@localhost>
2012-01-25 19:46:42 +0800

    metlib adding something new

---
+++ metlib/color/color_collections.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+reds = ['red', 'darkred', 'lightcoral', 'crimson', 'lightsalmon']
+oranges = ['orange', 'orangered', 'sandybrown', 'goldenrod', 'wheat']
+yellows = ['yellow', 'goldenrod', 'khaki', 'darkkhaki', 'moccasin']
+greens = ['green', 'darkgreen', 'yellowgreen', 'olive', 'lightgreen', 'lime']
+blues = ['blue', 'navy', 'royalblue', 'steelblue', 'skyblue', 'aqua']
+grays = ['gray', 'dimgray', 'lightgrey', 'darkslategray', 'slategray', 'black']
+greys = grays
+whites = ['white', 'mistyrose', 'honeydew', 'antiquewhite', 'beige']
+purples = ['purple', 'indigo', 'plum', 'blueviolet', 'mediumpurple']
+pinks = ['hotpink', 'mediumvioletred', 'pink', 'magenta', 'lightcoral']
+browns = ['sienna', 'saddlebrown', 'burlywood', 'brown', 'chocolate']
+
+seasons1 = []
+seasons2 = ['darkgreen', 'red', 'goldenrod', 'blue']
+seasons3 = ['lightgreen', 'salmon',  'khaki', 'deepskyblue']
+
+months1 = []
+months2 = []
+months3 = []
