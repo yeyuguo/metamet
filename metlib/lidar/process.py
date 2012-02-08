d0a58ab4c4bc85c2e66cb6e22e8a0ec03a15be21
claydodo <claydodo@localhost>
2012-02-08 21:53:36 +0800

    metlib.lidar added LidarPlot, color updated

--- metlib/lidar/process.py
+++ metlib/lidar/process.py
@@ -15,7 +15,8 @@ __all__ = ['correct_background', 'correct_afterpulse',
         'correct_overlap', 'correct_distance',
         'correct_energy',
         'fill_lower_part',
-        'zero_blind_range']
+        'zero_blind_range', 
+        'brutal_denoise', 'gentle_denoise', 'find_first_index_below_gate']
 
 def correct_background(data, sample_number=None):
     """corrects background noise.
@@ -94,6 +95,64 @@ def zero_blind_range(data):
         start_i = 0
     data['data'][...,:start_i] = 0.0
 
+def brutal_denoise(data, gate, **kwargs):
+    if isinstance(data, LidarDataset):
+        data = data['data']
+    indice = find_first_index_below_gate(data, gate, **kwargs)
+    if len(np.shape(indice)) == 2:
+        for t in range(data.shape[0]):
+            for c in range(data.shape[1]):
+                i = indice[t,c]
+                if i != -1:
+                    data[t, c, i:] = gate
+    elif len(np.shape(indice)) == 1:
+        for t in range(data.shape[0]):
+            i = indice[t]
+            if i != -1:
+                data[t, i:] = gate
+
+def gentle_denoise(data, gate, gate2=None, **kwargs):
+    if gate2 is None:
+        gate2 = gate
+    if isinstance(data, LidarDataset):
+        data = data['data']
+    indice = find_first_index_below_gate(data, gate, **kwargs)
+    if len(np.shape(indice)) == 2:
+        for t in range(data.shape[0]):
+            for c in range(data.shape[1]):
+                i = indice[t,c]
+                if i != -1:
+                    aver = np.ma.masked_invalid(data[t, c, i:]).mean()
+                    if aver < gate2:
+                        aver = gate2
+                    data[t,c,i:] = aver
+    elif len(np.shape(indice)) == 1:
+        for t in range(data.shape[0]):
+            i = indice[t]
+            if i != -1:
+                aver = np.ma.masked_invalid(data[t, i:]).mean()
+                if aver < gate2:
+                    aver = gate2
+                data[t,i:] = aver
+
+def find_first_index_below_gate(data, gate, start_i=0):
+    """find the index of first small value in lidar data.
+    data can be LidarDataset, array[TIME, CHANNEL, BIN] or array[TIME, BIN].
+    gate is the threshold.
+    start_i is the first value usable.
+    """
+    if isinstance(data, LidarDataset):
+        data = data['data']
+    # to treat nan as values below gate
+    cre = -(data >= gate)
+    cre[...,:start_i] = False
+    res = cre.argmax(axis=-1)
+    # for the case all cre value are false
+    if start_i > 0:
+        res[np.where(res==0)] = -1
+    else:
+        res[np.where((data[..., 0] >= gate) & (res == 0))] = -1
+    return res
 
 if __name__ == '__main__':
     pass
