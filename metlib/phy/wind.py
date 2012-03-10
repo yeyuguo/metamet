2e8e18e137213c2132e2c5b984839bc1cab2c19c
claydodo <claydodo@localhost>
2012-03-10 10:50:48 +0800

    metlib.phy.wind keeps scaler now

--- metlib/phy/wind.py
+++ metlib/phy/wind.py
@@ -1,16 +1,17 @@
 import numpy as np
 
 __all__ = ['wswd2uv', 'uv2wswd']
+
+# Using np.multiply and np.add and np.square to keep scaler.
 def wswd2uv(ws, wd):
     """Convert wind speed and wind direction to u, v.
     ws: wind speed
     wd: wind direction (in degrees, north wind is 0, east wind is 90, etc)
     Returns: u, v
     """
-    wd = np.deg2rad(np.array(wd))
-    ws = np.array(ws)
-    u = -ws * np.sin(wd)
-    v = -ws * np.cos(wd)
+    wd = np.deg2rad(wd)
+    u = -np.multiply(ws, np.sin(wd))
+    v = -np.multiply(ws, np.cos(wd))
     return u, v
 
 def uv2wswd(u, v):
@@ -20,9 +21,7 @@ def uv2wswd(u, v):
     ws: wind speed
     wd: wind direction (in degrees, north wind is 0, east wind is 90, etc)
     """
-    u = np.array(u)
-    v = np.array(v)
-    ws = np.sqrt(u ** 2 + v ** 2)
+    ws = np.sqrt(np.add(np.square(u), np.square(v)))
     wd = np.fmod(np.rad2deg(np.arctan2(u, v)) + 180.0 , 360.0)
     return ws, wd
 
