c5c02b07e47d69af302ea8e3aac1e1640cd5365b
claydodo <claydodo@localhost>
2012-06-19 13:57:05 +0800

    metlib.datetime.anomaly.py upgraded to data ndarray support

--- metlib/datetime/anomaly.py
+++ metlib/datetime/anomaly.py
@@ -20,7 +20,7 @@ from parser import *
 
 __all__ = ['three_month_anomaly']
 
-def three_month_anomaly(dts, data, beg_year, beg_month, end_year, end_month, day=1, least_days=30):
+def three_month_anomaly(dts, data, beg_year, beg_month, end_year, end_month, day=1, least_days=0):
     """three_month_anomaly calculates 3-month anomaly of given data series.
     dts: datetime seq.
     data: data seq.
@@ -36,7 +36,16 @@ def three_month_anomaly(dts, data, beg_year, beg_month, end_year, end_month, day
     beg_dts = tot_yms[:-3]
     mid_dts = tot_yms[1:-2]
     end_dts = tot_yms[3:]
-    stack = np.zeros((end_year-beg_year+1, 12), dtype='f8')
+    data_shp = np.shape(data)
+    stack_base_shape = (end_year-beg_year+1, 12)
+    result_base_shape = ((end_year-beg_year+1) * 12, )
+    if len(data_shp) > 1:
+        stack_shape = stack_base_shape + data_shp[1:]
+        result_shape = result_base_shape + data_shp[1:]
+    else:
+        stack_shape = stack_base_shape
+        result_shape = result_base_shape
+    stack = np.zeros(stack_shape, dtype='f8')
     stack[:] = np.nan
     for i in range(len(beg_dts)):
         beg = beg_dts[i]
@@ -44,10 +53,14 @@ def three_month_anomaly(dts, data, beg_year, beg_month, end_year, end_month, day
         yyyy = mid_dts[i].year
         mm   = mid_dts[i].month
         sel = np.ma.masked_invalid(data[np.where(datetime_is_between(beg, end, dts))])
-        if sel.count() >= least_days:
-            stack[yyyy-beg_year, mm-1] = sel.mean()
+        stack[yyyy-beg_year, mm-1] = sel.mean(axis=0)
+        if len(data_shp) > 1:
+            stack[yyyy-beg_year, mm-1, sel.count(axis=0) < least_days] = np.nan
+        else:
+            if sel.count() < least_days:
+                stack[yyyy-beg_year, mm-1] = np.nan
     basevalue = np.ma.masked_invalid(stack).mean(axis=0)
-    anomaly_stack = stack - basevalue
-    return anomaly_stack.flatten()[beg_month-1:beg_month-1+len(beg_dts)]
+    anomaly_stack = stack - basevalue[np.newaxis, ...]
+    return anomaly_stack.reshape(result_shape)[beg_month-1:beg_month-1+len(beg_dts)]
 
 
