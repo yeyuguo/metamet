06b31037820e07cca8ed070949c843effd9e2131
claydodo <claydodo@localhost>
2012-07-10 15:49:06 +0800

    metlib.shell.__init__ add .

--- metlib/shell/__init__.py
+++ metlib/shell/__init__.py
@@ -1,5 +1,5 @@
-from path import *
-from script_helper import *
-from fileutil import *
+from .path import *
+from .script_helper import *
+from .fileutil import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
