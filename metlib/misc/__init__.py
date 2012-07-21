f2811ef5ac0fa6f8b4e228cb7bb5184aeb038e1d
claydodo <claydodo@localhost>
2012-07-21 14:17:26 +0800

    metlib.misc add misc.py, including grep, strip_ext, get_ext, sub_ext

--- metlib/misc/__init__.py
+++ metlib/misc/__init__.py
@@ -2,5 +2,6 @@ from .simple_expression import *
 from .exp_interp import *
 from .spread_true_false import *
 from .alias import *
+from .misc import *
 
 __all__ = filter(lambda s:not s.startswith('_'),dir())
