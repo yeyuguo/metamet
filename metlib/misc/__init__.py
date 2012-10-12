547e59bc661e5f7cc24ea7ec4d6e1a1267b05c29
claydodo <claydodo@localhost>
2012-10-12 17:32:43 +0800

    misc add axgrid

--- metlib/misc/__init__.py
+++ metlib/misc/__init__.py
@@ -3,5 +3,5 @@ from .exp_interp import *
 from .spread_true_false import *
 from .alias import *
 from .misc import *
-
+from .axgrid import *
 __all__ = filter(lambda s:not s.startswith('_'),dir())
