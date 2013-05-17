bca88e06ac0474648a54307dce2cc2128c7f6ee6
claydodo <claydodo@localhost>
2013-05-17 14:41:00 +0800

    add Singleton and Null

--- metlib/__init__.py
+++ metlib/__init__.py
@@ -8,7 +8,8 @@ from metlib.data import rec_combine, rec_from_seqs, Basket, loadbasket, savebask
 from metlib.io.rec2csv_neat import rec2csv_neat
 from metlib.misc import ma_out, axgrid, axstack, grep, strip_ext, sub_ext, get_ext
 from metlib.misc import split_job, savepickle, loadpickle, struni, str2list
-from metlib.misc import limited_int, int_sign, second_derivate
+from metlib.misc import limited_int, Singleton, NullClass, Null
+from metlib.misc import int_sign, second_derivate
 from metlib.phy.wind import wswd2uv, uv2wswd
 from metlib.shell import force_makedirs, expand_path, find_link_orig, filesize, force_rm, list_all_file, get_output
 from metlib.color.color_collections import *
