dd9b3f15224d270c2dc622dfc186c4f834529066
claydodo <claydodo@localhost>
2013-12-11 10:13:32 +0800

    phy.pressure updated auto2K

--- metlib/phy/pressure.py
+++ metlib/phy/pressure.py
@@ -4,6 +4,6 @@ import numpy as np
 from .constants import *
 
 def dz_dp(p, Tv):
-    "-dz/dp. p:hPa, Tv: deg C, result: m/hPa"
-    return Rd*(Tv+273.15)/(p*9.80665)
+    "-dz/dp. p:hPa, Tv: either C/K is OK, result: m/hPa"
+    return Rd*auto2K(Tv)/(p*9.80665)
 
