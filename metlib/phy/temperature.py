1928d747fee5dea55f1a04d3d9da1fb0a77ae462
claydodo <claydodo@localhost>
2013-12-11 10:25:59 +0800

    Tv corrected

--- metlib/phy/temperature.py
+++ metlib/phy/temperature.py
@@ -14,7 +14,7 @@ def T2Tv(T, hum, humidity_type='RH', p=p_std, water_surface=True):
         e = hum_q2e(hum, p)
     else:
         pass
-    return (T+273.15) * (1.0-e/p*(1.0-epsilon_vapor)) - 273.15
+    return (T+273.15) / (1.0-e/p*(1.0-epsilon_vapor)) - 273.15
 
 def T2theta(T, p, p0=1000.0):
     "T to theta. T: deg C"
