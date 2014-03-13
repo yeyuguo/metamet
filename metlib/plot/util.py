81fc667ee6d42aa010f6355d7c993b68a9cff485
claydodo <claydodo@localhost>
2014-03-13 15:53:49 +0800

    metlib.plot.util add flip_legend() to plot legends by rows instead of by cols

--- metlib/plot/util.py
+++ metlib/plot/util.py
@@ -16,9 +16,10 @@ from matplotlib.ticker import FuncFormatter, FixedFormatter, ScalarFormatter, Lo
 #from mpl_toolkits.basemap import Basemap
 #from matplotlib import mlab
 #from netCDF4 import Dataset
+import itertools
 from metlib.misc.datatype import Null, isseq
 
-__all__ = ['format_ticks']
+__all__ = ['format_ticks', 'flip_legend']
 
 def format_ticks(ax=None, fmt=None, axis='x', fontsize=None, fontweight=None, **kwargs):
     """Format ticks.
@@ -73,6 +74,20 @@ def format_ticks(ax=None, fmt=None, axis='x', fontsize=None, fontweight=None, **
 
     if fontweight:
         plt.setp(theaxis.get_ticklabels(), fontweight=fontweight)
+
+def flip_legend(ax, *args, **kwargs):
+    """flip_legend() plots the legend of an axes as plt.legend(), but the labels fills rows first.
+    According to Avaris' solution on stackoverflow.com:
+    http://stackoverflow.com/questions/10101141/matplotlib-legend-add-items-across-columns-instead-of-down
+    """
+    def flip(items, ncol):
+        return itertools.chain(*[items[i::ncol] for i in range(ncol)])
+    
+    ncol = kwargs['ncol'] if 'ncol' in kwargs else 1
+    handles, labels = ax.get_legend_handles_labels()
+    flipped_handles = flip(handles, ncol)
+    flipped_labels = flip(labels, ncol)
+    ax.legend(flipped_handles, flipped_labels, *args, **kwargs)
 
 if __name__ == '__main__':
     pass
