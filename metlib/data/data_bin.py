6b64e6b0ee6e4d7e0e6dfd3cb642d47872b99343
claydodo <claydodo@localhost>
2014-04-18 14:39:20 +0800

    metlib.data.data_2d_bin.py depreciated, moved into data_bin.py; data_bin.py add calc_bins(bins, func, dtype)

--- metlib/data/data_bin.py
+++ metlib/data/data_bin.py
@@ -6,7 +6,7 @@
 import os, sys
 import numpy as np
 
-__all__ = ['data_bin'] 
+__all__ = ['data_bin', 'data_2d_bin', 'calc_bins'] 
 def data_bin(data, binsize, start=None, end=None, return_where=False, return_bin_info=False):
     """This function partitions a seq of data into equal bin. 
 
@@ -61,9 +61,57 @@ def data_bin(data, binsize, start=None, end=None, return_where=False, return_bin
         return result
 
 
+def data_2d_bin(data, xs, ys, x_points, y_points):
+    """This function partitions a seq of data with coordinates (xs, ys), into 2d bins defined by split points (x_points, y_points). 
+
+Parameters:
+    data: a seq of data.
+    xs, ys: seqs of coordinates of the data.
+    x_points, y_points: seqs of split points. 
+Returns:
+    A 2d list with the shape of (len(y_points)-1, len(x_points)-1).
+    """
+    assert len(data) == len(xs) == len(ys)
+    if len(data) == 0:
+        return [[[]]]
+    res = []
+    for jy in range(len(y_points)-1):
+        to_append = []
+        for ix in range(len(x_points)-1):
+            to_append.append([])
+        res.append(to_append)
+
+    for jy in range(len(y_points)-1):
+        wy = np.where( (y_points[jy] <= ys) & (ys < y_points[jy+1]) )
+        sub_data = data[wy]
+        sub_xs = xs[wy]
+        for ix in range(len(x_points)-1):
+            wx = np.where( (x_points[ix] <= sub_xs) & (sub_xs < x_points[ix+1]) )
+            res[jy][ix].extend(sub_data[wx])
+
+    return res
+
+def calc_bins(bins, func, dtype='f8'):
+    """calc each bin with func, return an array."""
+    res_shape = np.shape(bins)[-1]
+    res = np.zeros(res_shape, dtype='O')
+    for index in np.ndindex(res_shape):
+        the_bin = bins
+        for i in index:
+            the_bin = the_bin[i]
+        res[index] = func(the_bin)
+    return res.astype(dtype)
+
 if __name__ == '__main__':
     a = np.random.rand(100)
     b, i = data_bin(a, 0.1, 0.0, 1.0, return_bin_info=True)
     w = data_bin(a, 0.2, 0.0, 1.0, return_where=True)
     print b, i
     print w
+
+    a = np.random.rand(100)
+    xs = np.random.rand(100) * 100.0
+    ys = np.random.rand(100) * 100.0
+    print xs, ys
+    print data_2d_bin(a, xs, ys, [0, 20, 50, 100], [24, 50, 100])
+
